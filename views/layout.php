ed4a1ac1b64c9cef323add12cbc0112c5480328d
Krešimir Mihalj <kmihalj@srce.hr>
2025-09-28 16:17:34 +0200

    layout comment Local/CDN bootstrap

--- views/layout.php
+++ views/layout.php
@@ -23,6 +23,7 @@ use App\Core\App;
 use App\Core\I18n;
 
 ?>
+<!-- Local version -->
 <!DOCTYPE html>
 <html lang="<?= htmlspecialchars(I18n::getLocale(), ENT_QUOTES, 'UTF-8') ?>">
 <head>
@@ -38,6 +39,30 @@ use App\Core\I18n;
   <script
     src="<?= App::url('js/bootstrap.bundle.min.js') ?>?v=<?= filemtime(__DIR__ . '/../public/js/bootstrap.bundle.min.js') ?>"></script>
 </head>
+
+<!-- CDN Version -->
+<!--
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php /*= isset($title) ? htmlspecialchars($title) : _t('Aplikacija') */?></title>
+  <base href="<?php /*= App::baseHref(); */?>">
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous">
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"
+    rel="stylesheet">
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</head>
+-->
+
+
 <body>
 <?php include __DIR__ . '/menu.php'; ?>
 
